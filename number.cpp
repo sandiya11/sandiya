92c9d03a16170076f8cec9dedbcfdddb6dfa4fb1
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-02-15 09:49:13 +0530

    Create number.cpp

---
+++ number.cpp
@@ -0,0 +1,19 @@
+int main() {
+	int a;
+	cout<<"enter the values\n";
+	cin>>a;
+	if(a>0)
+	{
+		cout<<"a is positive";
+	
+	}
+	else if(a<0)
+	{
+		cout<<"a is negative";
+	
+	}
+	else// your code goes here
+	{
+		cout<<"a is zero";
+		}return 0;
+}
