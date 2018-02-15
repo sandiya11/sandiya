881b0170d97c51f1f8f271f7c20c1a608d108d9e
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-02-15 10:43:47 +0530

    Create even.cpp

---
+++ even.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()  
+{
+	int a;
+	cout<<"enter the values\n";
+	cin>>a;
+	if(1<=a<=10000)
+	
+	if(a%2==0)
+	{
+		cout<<" even";
+	
+	}
+	else 
+	{
+		cout<<"odd";
+}
+}
