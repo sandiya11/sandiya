0d15aa31fdf86e3916655174e1ec5e7bfc8d3f80
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-02-15 10:25:17 +0530

    Create num.cpp

---
+++ num.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()  
+{
+	int a;
+	cout<<"enter the values\n";
+	cin>>a;
+	
+	if((a<=1)&&(a>=100000))
+	{
+		cout<<"a is positive";
+	
+	}
+	else 
+	{
+		cout<<"ais negaive or zero";
+}
+}
