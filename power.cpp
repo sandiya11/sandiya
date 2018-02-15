cf433c763d65e818dea7be83995ae65e932bff00
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-02-15 15:52:27 +0530

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,i,p=1;
+	cout<<"enter the value\n";
+	cin>>a>>b;
+	for(i=1;i<=b;i++)
+	{
+	p=p*a;
+	}
+	cout<<p;
+	// your code goes here
+	return 0;
+}
