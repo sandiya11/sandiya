01983454807f3218bcd2fb92a25bc8fd4b283581
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-02-15 11:46:40 +0530

    Create vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()  
+{
+	char C,i;
+	char ch[10]={'a','e','i','o','u','A','E','I','O','U'};
+	cout<<"enter the letter\n";
+	cin>>C;
+	if(ch[i]==C)
+	{
+	cout<<"the letter is vowel";
+	}
+else
+{
+	cout<<"the letter is constant";
+}
+}
