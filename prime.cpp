5cd4ae6278a9398e432a8d862bd1338eca127904
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-02-21 16:33:23 +0530

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+
+int n;
+cout << "Enter an integer "<<endl;
+cin>>n;
+
+
+if (n%2==0)
+cout << n<< "it is not prime number" << endl;
+
+
+else if (n%2==1)
+cout<<n<<" it is a prime number"<< endl;
+
+
+
+return 0;
+
+}
