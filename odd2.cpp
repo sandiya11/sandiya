ebbc4836a03feebd136f851746096b7fdbb48aef
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-02-24 15:26:16 +0530

    Create odd2.cpp

---
+++ odd2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+    int num,m,n;
+    cout<<"enter the number\n";
+    cin>>m>>n;
+for (num = m; num <= n; num++)
+
+        {
+
+               if (num % 2 == 1)
+
+                cout<<"\nthe odd number is\n"<<num;
+
+         }
+
+                return 0;
+
+}
