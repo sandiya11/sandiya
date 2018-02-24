a0c776234d615cb570258a88433eabc4b86754ac
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-02-24 16:11:57 +0530

    Create armstrong.cpp

---
+++ armstrong.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int n, m, num=0, rem;
+	cout<<"Enter the number :\n ";
+	cin>>n;
+	 m=n;
+	while(m!=0)
+	{
+		rem=m%10;
+		num=num + rem*rem*rem;
+		m=m/10;
+	}
+	if(num==n)
+	{
+		cout<<"Armstrong Number";
+	}
+	else
+	{
+		cout<<"Not a Armstrong Number";
+	}
+	return 0;
+}
