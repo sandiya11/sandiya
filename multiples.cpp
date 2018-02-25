1048b29c6b95a4e94ffa457ee74635841992bd03
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-02-25 11:33:57 +0530

    Create multiples.cpp

---
+++ multiples.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+           
+     int n,i;
+                
+     cout<<"\n Enter a  number\n";
+     cin>>n;
+     for(i=1;i<=5;i++)
+      {
+       cout<<("\n n * n= n ",n,i,n*i)<<" ";
+      }
+      return 0;
+}
