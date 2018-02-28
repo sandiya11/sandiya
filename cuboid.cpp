e0e4a6035105a71f85f8736db1e3f79459beafaa
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-02-28 21:15:54 +0530

    Update cuboid.cpp

--- cuboid.cpp
+++ cuboid.cpp
@@ -7,8 +7,8 @@ int l,b,h,v,tsa;
 cout<<"enter the value :\n";
 cin>>l>>b>>h;
 v=l*b*h;
-tsa=2((l*b)+(b*h)+(h*l));
-cout<<"volume of cuboid="<<v;
+tsa=2*((l*b)+(b*h)+(h*l));
+cout<<"volume of cuboid="<<v<<endl;
 cout<<"total surface area of cuboid="<<tsa;
 return 0;
 }
