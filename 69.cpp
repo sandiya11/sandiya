bb99a2adb3a3241191079742d0209b09c603db01
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-01 20:32:04 +0530

    Create 69.cpp

---
+++ 69.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"enter the value"<<endl;
+	cin>>a>>b;
+	c=a+b;
+	if(c%2==0)
+	{
+		cout<<"it is a even"<<endl;
+	}
+	else
+	{
+		cout<<"it is a odd"<<endl;
+	}
+	return 0;
+}
