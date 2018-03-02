833fb3e4be1907c4792cc78aae1fb06c2fba97d2
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-02 19:41:10 +0530

    Create 32.cpp

---
+++ 32.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int a,b;
+	cout<<"enter the value:\n";
+	cin>>a>>b;
+	a=a^b;
+	b=a^b;
+	a=a^b;
+	cout<<"after wapping"<<endl;
+	cout<<a<<endl<<b<<endl;
+  return 0;
+}
