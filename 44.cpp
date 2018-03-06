9bf28ffd2d33e6a5d8d25add771eb25e28ded451
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-06 21:33:55 +0530

    Create 44.cpp

---
+++ 44.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int n;
+	cout<<"enter the number:";
+	cin>>n;
+	if(n>=1 && n<=10)
+	{
+	cout<<" yes";
+	}
+	else
+	{
+	cout<<"no";
+	}
+	return 0;
+}
