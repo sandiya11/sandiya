6db8b62dfb6b63c45efae0ab90644f58a26b7b04
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-07 19:33:35 +0530

    Create 68.cpp

---
+++ 68.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int n;
+cout<<"enter the number"<<endl;
+cin>>n;
+if(n%7==0)
+{
+	cout<<"yes";
+}
+else
+{
+	cout<<"no";
+}
+	return 0;
+}
