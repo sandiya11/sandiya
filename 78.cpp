091a40392c6d29b1fc26c19470badd1b50c92905
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-07 06:03:39 +0530

    Create 78.cpp

---
+++ 78.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int n,i;
+	cout<<"enter the number"<<endl;
+	cin>>n;
+	if(n%13==0)
+	{
+   cout<<"yes";
+	}
+	else
+	{
+	cout<<"no";
+	}
+	return 0;
+}
