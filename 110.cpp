a6f358359dba1dd5ff95e8b31f478d5a6e0f8acb
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-07 20:01:17 +0530

    Create 110.cpp

---
+++ 110.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,i,sum;
+	cout<<"enter the number:"<<endl;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		sum=sum+i;
+	}
+	cout<<sum;
+	return 0;
+}
