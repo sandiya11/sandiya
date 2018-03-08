c37aadd223900680e15f558476004c9b931de5de
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-08 22:17:48 +0530

    Create 96.cpp

---
+++ 96.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+        int n, i, count=0;
+        cout<< "Enter the number:\n ";
+        cin>>n;
+        for(i=2;i<n;i++)
+        {
+        if(n%i==0)
+        {
+        count++;
+        }
+        }
+        if(count!=0)
+        {
+        cout<<"composite Number";
+        }
+        else
+        {
+        cout<<"not composite number";
+        }
+        return 0;
+}
