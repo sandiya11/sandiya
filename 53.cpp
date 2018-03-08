2a68f052b310fb5317a8009a575ad2462f85e435
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-08 22:37:00 +0530

    Create 53.cpp

---
+++ 53.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,remainder,sum=0;
+	cin>>n;
+	while(n!=0)
+	{
+		remainder=n%10;
+		sum=sum+remainder;
+		n=n/10;
+	}
+	cout<<sum;
+	return 0;
+}
