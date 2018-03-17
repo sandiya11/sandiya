69c0e6a6ec33b9b793a73ea92e7fbb232abfc9e6
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-17 11:15:22 +0530

    Create 61.cpp

---
+++ 61.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	char str[20];
+	int k,i;
+	cout<<"enter the string"<<endl;
+	cin>>str;
+	cout<<"enter the values to print"<<endl;
+	cin>>k;
+	for(i=0;i<k;i++)
+	{
+	cout<<str[i];
+	}
+	return 0;
+}
+	
