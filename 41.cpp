474cb5e81bf3473acee7bd50d498ff132e4060f8
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-19 14:06:32 +0530

    Create 41.cpp

---
+++ 41.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int num,i;
+char a[50];
+cout<<"Enter the string"<<endl;
+cin>>a;
+cout<<"enter the value to print"<<endl;
+cin>>num;
+for(i=1;i<=num;i++)
+{
+cout<<a<<endl;
+}
+return 0;
+}
