4596ad3d2204c90ea089522d85343b385865f690
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-20 10:21:03 +0530

    Create 60.py

---
+++ 60.py
@@ -0,0 +1,13 @@
+n = int(input("\nEnter the Range:\n"))
+i = 0
+n1 = 0
+n2 = 1
+while(i <n):
+           if(i <= 1):
+                      Next = i
+           else:
+                      Next =n1 + n2
+                      n1= n2
+                      n2= Next
+           print(Next)
+           i =i+1
