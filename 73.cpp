034bd5f8cf207decafe64034e1f9008ddae55022
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-03-25 13:19:00 +0530

    Create 73.cpp

---
+++ 73.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+using namespace std;
+int main() 
+{
+	int m,n,r;
+	cout<<"enter the number"<<endl;
+	cin>>r;
+	cout<<"enter two numbers"<<endl;
+	cin>>n>>m;
+	if(r>n && r<m)
+	{
+	cout<<"yes";
+	}
+	else
+	{
+    cout<<"no";
+	}
+	return 0;
+}
