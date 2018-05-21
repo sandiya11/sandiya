df0e461da96757f599f4e707a9892dbbb44bb387
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-05-21 12:36:36 +0530

    Create 112.cpp

---
+++ 112.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,k,a[10],i;
+	cin>>n>>k;
+	for(i=0;i<n;i++)
+	{
+	cin>>a[i];
+		
+	}
+     for(i=0;i<n;i++)
+      {
+	if(k==a[i])
+	{
+		cout<<"yes";
+		
+	}
+}
+cout<<"no";
+	return 0;
+}
