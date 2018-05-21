54ae75742c70ba36f9ae23f6441f0ac24287db07
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-05-21 11:32:54 +0530

    Create 83.cpp

---
+++ 83.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c,d;
+	cin>>a>>b;
+	if((a>0)&&(b>0))
+	{
+	c=a / b;
+	cout<<c<<endl;
+	d=a % b;
+	cout<<d<<endl;
+	}
+	
+	return 0;
+}
