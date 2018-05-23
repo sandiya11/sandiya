69bfb8aa571b4ffcbd44f4a282bb4fbac3cc4471
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-05-23 12:27:03 +0530

    Create p11.cpp

---
+++ p11.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+ string a;
+ cin>>a;
+ if(a=="sunday"||a=="saturday")
+ {
+     cout<<"yes";
+ }
+ else
+ {
+     cout<<"no";
+ }
+ 
+}
