cd0d9092ee75258e43317d828b3374e4c77c6019
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-05-23 15:45:02 +0530

    Create 208.cpp

---
+++ 208.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+# include<math.h>
+
+using namespace std;
+
+int main() 
+{
+    int a,b,c,d,ans;
+    cin>>a>>b;
+    if(a,b,c<=100000)
+    {
+    d=pow(a,b);
+    ans=d%c;
+    cout<<ans;
+    }
+    else
+    {
+    cout<<"invalid number";
+    }
+}
