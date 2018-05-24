24a3bc013b3edd967e8da312956367592d0495e5
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-05-24 11:13:28 +0530

    Create 188.cpp

---
+++ 188.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cin>>a>>b>>c;
+if(a,b,c<=100000)
+{
+    if(a+b>c)
+    {
+        cout<<"yes";
+        
+    }
+    else
+    {
+    cout<<"no";
+    }
+}
+else
+{
+    cout<<"invalid number";
+}
+}
